f746f8d0233b350d0a19bc643f32785b3915411f
Russell Hayes <info@rahphotographyco.com>
2018-01-04 15:11:02 -0700

    adding items to firebase database

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import firebase from './fire';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       username: '',
-      currentItem: ''
+      currentItem: '',
+      items: []
     }
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
     this.setState({
       [e.target.name]: e.target.value
+    });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    const itemsRef = firebase.database().ref('items');
+    const item = {
+      title: this.state.currentItem,
+      user: this.state.username
+    }
+    itemsRef.push(item);
+    this.setState({
+      currentItem: '',
+      username: ''
     });
   }
 
@@ -29,9 +45,9 @@ class App extends Component {
         </header>
         <div className='container'>
           <section className='add-item'>
-            <form>
+            <form onSubmit={this.handleSubmit}>
               <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
-              <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
+              <input type="text" name="currentItem" placeholder="What are you bringing ?" onChange={this.handleChange} value={this.state.currentItem} />
               <button>Add Item</button>
             </form>
           </section>
